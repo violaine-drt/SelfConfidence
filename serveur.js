e9aa980b41554147cf4ba384f6ca62370599f4bb
violaine-drt <viodarrent@gmail.com>
2024-02-27 16:15:17 +0100

    Ajout de add.post

--- serveur.js
+++ serveur.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const port = process.env.PORT || 5000;
 const app = express();
+app.use(express.json())
 
 
 app.listen(port, () => {
-    console.log("Serveur is online");
-})
-
+        console.log("You are on PORT", port);
+}
+)
 
 async function citation() {
     let response = await fetch('https://www.affirmations.dev/');
@@ -15,9 +16,16 @@ async function citation() {
     return result;
 }
 
- 
+
 //Définir la doc et les endpoints de son API (comment on va utiliser les verbes get, post, put...)
 app.get("/", async (req, res) => {
     let result = await citation()
     res.send(result)
+})
+
+
+app.post ("/", async (req, res) => {
+    let request = await req.body
+    console.log(request)
+    res.send(request)
 })
